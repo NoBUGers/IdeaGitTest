a1fe687467df7351764f0091661e35186073bd0d
XiFan888 <2586903364@qq.com>
2021-12-08 10:19:45 +0800

    更新1

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -10,5 +10,6 @@ import java.sql.SQLOutput;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("更新1！");
     }
 }
