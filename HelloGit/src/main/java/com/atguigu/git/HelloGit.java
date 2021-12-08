a002b3ea9627dbb9ae84dd5f6c5060fa767edffa
XiFan888 <2586903364@qq.com>
2021-12-08 10:18:06 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author xifan Email:2586903364@qq.com
+ * @Description
+ * @date
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
